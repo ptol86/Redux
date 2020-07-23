7cfc9411fd4fa0363db77245052c9863913e1d3b
ptol86 <ah021714@gmail.com>
2020-07-23 15:31:58 +0300

    test

---
+++ lesson2/task2/src/index.jsx
@@ -0,0 +1,29 @@
+import './index.scss';
+import  store, {increment, decrement, reset} from './store';
+ 
+const resultElem = document.querySelector(".counter__result");
+const incrementBtn = document.querySelector(`[data-action="increment"]`);
+const decrementBtn = document.querySelector(`[data-action="decrement"]`);
+const resetBtn = document.querySelector(`[data-action="reset"]`);
+
+
+const onIncrement = () => {
+    store.dispatch(increment())
+}
+const onDecrement = () => {
+    store.dispatch(decrement())
+}
+const onReset = () => {
+    store.dispatch(reset())
+}
+
+incrementBtn.addEventListener("click", onIncrement)
+decrementBtn.addEventListener("click", onDecrement)
+resetBtn.addEventListener("click", onReset)
+
+store.subscribe(() => {
+    const state = store.getState();
+    const currentValue = state.value;
+    const historyString = state.history.join("");
+    resultElem.textContent = `${historyString} = ${currentValue}`;
+})
