12a6d9330bcefc65ed59f07092a33cb3bbd47570
ptol86 <ah021714@gmail.com>
2020-07-25 15:58:27 +0300

    test

---
+++ lesson3/task1/src/counter.reducer.js
@@ -0,0 +1,29 @@
+import {INCREMENT, DECREMENT, RESET } from "./counter.actions"
+
+const initialState = {
+    counter: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case INCREMENT:
+            return {
+                ...state,
+                counter: state.counter + 1,
+            };
+        case DECREMENT:
+            return {
+                ...state,
+                counter: state.counter - 1,
+            };
+        case RESET:
+            return {
+                ...state,
+                counter: 0,
+            };
+        default: 
+            return state;
+    }
+}
+
+export default counterReducer;
---
+++ lesson3/task1/src/index.jsx
@@ -0,0 +1,39 @@
+import  store  from './store';
+import  { deleteUser, addUser, updateUser }  from './users.actions';
+import  {increment, decrement, reset} from './counter.actions';
+
+
+// const user = { id: 76, name: 'Sarah' };
+// const user2 = { id: 716, name: 'Tom' };
+
+// const onAddUser = (user) => {
+//     store.dispatch(addUser(user))
+// }
+// onAddUser(user)
+// onAddUser(user2)
+
+// // const onDeleteUser = (id) => {
+// //     store.dispatch(deleteUser(id))
+// // }
+// const onUpdateUser = (id, userData) => {
+//     store.dispatch(updateUser(id, userData))
+// }
+// onUpdateUser(716, {name: "John"})
+
+// // console.log(store.getState())
+// // console.log(onAddUser.prototype)
+// // store.subscribe(() => {
+// //     const state = store.getState();
+// //     const currentValue = state.history.reduce((acc, value) => acc + Number(value), 0);
+// //     const historyString = state.history.join("");
+// //     resultElem.textContent = state.history.length === 0
+// //     ? ""
+// //     : `${historyString} = ${currentValue}`;
+        
+// // })
+
+// store.dispatch(increment())
+// store.dispatch(increment())
+// store.dispatch(increment())
+
+// console.log((store.getState()))
