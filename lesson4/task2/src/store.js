ebc8b855d1d220e1b7cfc5df4783e310a4178c79
ptol86 <ah021714@gmail.com>
2020-07-27 22:18:20 +0300

    test

---
+++ lesson4/task2/src/store.js
@@ -0,0 +1,36 @@
+import { createStore, combineReducers } from 'redux';
+import { usersReducer } from "./users/users.reducer";
+
+// // const reducer = combineReducers({
+// //     users: usersReducer,
+// //     // counter: counterReducer
+// // });
+
+const store = createStore(
+  usersReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
+
+
+
+
+
+
+
+// import { createStore } from 'redux';
+// // import counterReducer from './counter/counter.reducer';
+// import usersReducer from './users/users.reducer';
+// import { createStore, combineReducers } from 'redux';
+
+
+// // const reducer = combineReducers({
+// //     users: usersReducer,
+// //     // counter: counterReducer
+// // });
+
+// const store = createStore(usersReducer,
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+// export default store;
---
+++ lesson4/task2/src/users/Users.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as userActions from "./users.actions";
+
+class Users extends Component {
+    onUserCreate = () => {
+        const id = Math.round(Math.random() * 1000000);
+        const newUser = {
+        id,
+        name: `User # ${id}`,
+        };
+        this.props.addUser(newUser);
+    };
+
+    render() {
+        const { users, deleteUser } = this.props;
+        return (
+            <div className="users">
+                <button 
+                    onClick={this.onUserCreate} 
+                    className="users__create-btn"
+                >
+                    Create user
+                </button>
+                <ul className="users__list">
+                    {users.map((user) => (
+                    <li key={user.id} className="users__list-item">
+                        {user.name}
+                        <button
+                            onClick={() => deleteUser(user.id)}
+                            className="users__delete-btn"
+                        >
+                            +
+                        </button>
+                    </li>
+                    ))
+                    }
+                </ul>
+            </div>
+        );
+    }
+}
+
+const mapState = (state) => {
+    return {
+        users: state.usersList,
+    };
+};
+
+const mapDispatch = {
+    addUser: userActions.addUser,
+    deleteUser: userActions.deleteUser,
+};
+
+const connector = connect(mapState, mapDispatch);
+const connectedUsers = connector(Users);
+
+export default connectedUsers;
